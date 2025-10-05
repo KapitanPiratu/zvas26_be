9483ecd29bd9d4f3b7f6a36b2181ba27c3cc2eec
KapitanPiratu <janek.marek@outlook.cz>
2025-10-05 21:28:59 +0200

    get /teams now returns points for each team

--- index.js
+++ index.js
@@ -14,11 +14,36 @@ app.get("/", (req, res) => {
 app.get("/teams", async (req, res) => {
     /**
      * Returns teams from db.
+     *
+     * First selects all teams from db.
+     * Then queries task_log for each team and calculates points from completed tasks.
      */
+
     console.log(`GET request to /teams from ${req.ip}`);
 
     try {
         const rows = await all("SELECT * FROM team");
+
+        for (const team of rows) {
+            team.points = 0;
+            const tasks = await all(
+                `SELECT
+                    task_log.*, task.points
+                FROM
+                    task_log
+                LEFT JOIN
+                    task
+                ON
+                    task.id = task_log.task_id
+                WHERE
+                    team_id = ?`,
+                [team.id]
+            );
+
+            tasks.forEach((task) => {
+                if (task.completed) team.points += task.points;
+            });
+        }
 
         res.send(rows);
     } catch (err) {
