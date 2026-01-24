46a88bf8d2b66fbca729432f3cfb42e67e6addeb
KapitanPiratu <janek.marek@outlook.cz>
2026-01-24 19:07:25 +0100

    added initialization of checkpoints

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { getTeams } = require("./routes/getteams.js");
 const { getTasks } = require("./routes/gettasks.js");
 const { postTaskslog } = require("./routes/posttaskslog.js");
 const { postArrivallog } = require("./routes/postarrivallog.js");
+const { checkpointCheck } = require("./routes/checkpointcheck.js");
 
 app.use(express.json());
 app.use(cors());
@@ -28,6 +29,10 @@ app.post("/taskslog", (req, res) => {
 
 app.post("/arrivallog", (req, res) => {
     postArrivallog(req, res);
+});
+
+app.post("/checkpoint", (req, res) => {
+    checkpointCheck(req, res);
 });
 
 const PORT = process.env.BE_PORT || 3001;
